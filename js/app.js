775a427b8b6212f38d85405029dc78e5596f0f71
Taz Strohmayer <taz@Tazs-Work-Mac-2.lan>
2019-03-01 16:54:39 -0800

    -Fix buttons reporting correct element name every time (css: pointer-events: none) -Merge jquery button event for all buttons. Parsing from there.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 // $(".launch-page").hide();
 // $(".main-page").hide();
 
-
-$(".launch-mode-btn").on("click", function(event) {
+$('.btn').on('click', function(event){
   console.log(event.target.name);
-  $(".launch-page").fadeOut(200);
-  $(".main-page").fadeIn(200);
-});
 
-$('.btn[name="power"]').on("click", function(event){
-    console.log(event.target.name);
+  // PAGE FLIPS //
+  if (event.target.name === 'power') {
     $(".launch-page").fadeIn(200);
     $(".main-page").fadeOut(200);
+  }
+
+  if (event.target.className === 'btn launch-mode-btn') {
+    $(".launch-page").fadeOut(200);
+    $(".main-page").fadeIn(200);
+  }
+
+
+  
 });

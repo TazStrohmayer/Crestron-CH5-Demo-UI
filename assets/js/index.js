9e1549da57361d6e292cec007b7ebc3f02c682de
Taz Strohmayer <taz@pawping.com>
2021-04-19 13:16:47 -0500

    - Add document.ready and put all code inside that just to see if it would help publishEvent

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,47 @@
-import * as CrComLib from "@crestron/ch5-crcomlib";
+import { publishEvent } from '@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib';
+
+$(document).ready(function() {
+  goToLaunch();
+
+  $(".btn").on("click", function(event) {
+    console.log(event.target.name);
+
+    // PAGE FLIPS //
+    if (event.target.name === "power") {
+      goToLaunch();
+    }
+
+    if (event.target.className === "btn launch-mode-btn") {
+      goToMain();
+    }
+
+    // SUB PAGE FLIPS //
+    if (event.target.name === "present") {
+      $(".present-sub").fadeIn();
+      $(".atc-sub").hide();
+      $(".vtc-sub").hide();
+    }
+
+    if (event.target.name === "atc") {
+      $(".present-sub").hide();
+      $(".atc-sub").fadeIn();
+      $(".vtc-sub").hide();
+    }
+
+    if (event.target.name === "vtc") {
+      $(".present-sub").hide();
+      $(".atc-sub").hide();
+      $(".vtc-sub").fadeIn();
+    }
+
+    // BUTTON PRESSES //
+    if (event.target.name === "privacy") {
+      publishEvent("b", "1", true);
+      setTimeout(() => publishEvent("b", "1", false), 200);
+    }
+  });
+});
+
 
 // Animations
 function animateCSS(element, animation, duration, prefix = 'animate__', callback) {
@@ -39,41 +82,3 @@ function goToMain() {
   animateCSS('.main-footer-right','slideInUp', '0.6s');
 }
 
-goToLaunch();
-
-$(".btn").on("click", function(event) {
-  console.log(event.target.name);
-
-  // PAGE FLIPS //
-  if (event.target.name === "power") {
-    goToLaunch();
-  }
-
-  if (event.target.className === "btn launch-mode-btn") {
-    goToMain();
-  }
-
-  // SUB PAGE FLIPS
-  if (event.target.name === "present") {
-    $(".present-sub").fadeIn();
-    $(".atc-sub").hide();
-    $(".vtc-sub").hide();
-  }
-
-  if (event.target.name === "atc") {
-    $(".present-sub").hide();
-    $(".atc-sub").fadeIn();
-    $(".vtc-sub").hide();
-  }
-
-  if (event.target.name === "vtc") {
-    $(".present-sub").hide();
-    $(".atc-sub").hide();
-    $(".vtc-sub").fadeIn();
-  }
-
-  if (event.target.name === "privacy") {
-    CrComLib.publishEvent("b", "1", true);
-    setTimeout(() => CrComLib.publishEvent("b", "1", false), 200);
-  }
-});
